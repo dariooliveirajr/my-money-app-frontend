55ec40135edc620b255997e43bd43b161b96f3d1
Dário Oliveira Júnior <juniordario06@gmail.com>
2020-11-23 12:09:12 -0300

    att

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
 export default {
-    API_URL: 'http://dariooliveirajr.com/my-money-app-backend/api',
-    OAPI_URL: 'http://dariooliveirajr.com/my-money-app-backend/oapi',
+    API_URL: 'http://dariooliveira.com/my-money-app-backend/api',
+    OAPI_URL: 'http://dariooliveira.com/my-money-app-backend/oapi',
   }
